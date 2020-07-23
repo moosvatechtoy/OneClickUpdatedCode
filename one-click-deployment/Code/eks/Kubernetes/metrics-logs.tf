1919af2cb8d2941717899cfd5d00aa73e1340f67
Sasidhara Babu Navuluri <nsasidhara@pkglobal.com>
2020-07-23 17:23:16 +0530

    updated back the code removed env variables

--- one-click-deployment/Code/eks/Kubernetes/metrics-logs.tf
+++ one-click-deployment/Code/eks/Kubernetes/metrics-logs.tf
@@ -1,20 +1,11 @@
 
-resource "null_resource" "exportvariables" {
-  provisioner "local-exec" {
-    command = "export AWS_ACCESS_KEY_ID=${var.access_key} & export AWS_SECRET_ACCESS_KEY=${var.secret_key}"
-  }
-  depends_on = [
-    null_resource.config-temp
-  ]
-}
-
 resource "null_resource" "container-insight" {
   provisioner "local-exec" {
     #command = "kubectl apply -f -<<EOF\n${data.template_file.metrics.rendered}\nEOF"
     command = "curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluentd-quickstart.yaml | sed \"s/{{cluster_name}}/${aws_eks_cluster.setup_step.id}/\" | sed \"s/{{region_name}}/${data.aws_region.current.id}/\" | kubectl apply -f -"
   }
   depends_on = [
-    null_resource.exportvariables
+    null_resource.config-temp
   ]
 }
 
