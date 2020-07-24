f1b22a87b0d19d3dd8f98e96693a887cdd0eee62
Sasidhara Babu Navuluri <nsasidhara@pkglobal.com>
2020-07-24 16:32:51 +0530

    activating the Service Account to run from any instance

--- one-click-deployment/Code/gke/Kubernetes/kube-config.tf
+++ one-click-deployment/Code/gke/Kubernetes/kube-config.tf
@@ -4,6 +4,11 @@ resource "null_resource" "config-temp" {
      command = "[ -f $HOME/.kube/config ] && mv $HOME/.kube/config $HOME/.kube/config_$(date +%F-%H:%M) || echo \"config file does not exist.\""
   } 
 
+   # Activate the Service account
+   provisioner "local-exec" {
+        command = "gcloud auth activate-service-account --key-file=${var.credentials}"
+  }
+
    # Copies the kubeconf file to ~/.kube/config
    provisioner "local-exec" {
         command = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.gcp_location} --project ${var.project_id}"
